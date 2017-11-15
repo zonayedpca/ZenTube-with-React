d57e6e5c3ecc8cf7d375ae15bb2c9c1b7621e218
Zonayed Ahmed <zonayedpca@yahoo.com>
2017-11-15 06:56:12 +0530

    basic app mockup done

---
+++ src/Components/video-details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoDetails = () => {
+	return (
+		<div className="col-md-8">
+			<p>VideoDetails will go here!</p>
+		</div>
+	)
+}
+
+export default VideoDetails;
---
+++ src/Components/video-list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const VideoList = () => {
+	return (
+		<div className="col-md-4">
+			<ul className="list-group">
+				<li>List 1</li>
+				<li>List 1</li>
+				<li>List 1</li>
+				<li>List 1</li>
+				<li>List 1</li>
+			</ul>
+		</div>
+	)
+}
+
+export default VideoList;
